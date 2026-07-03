bdfe539baa3a067aca89a401f6dd5c6b05d77988
Antoine GROUAZEL <antoine.grouazel@ifremer.fr>
2026-07-03 16:26:04 +0200

    add docker stuff and separate web from catalogue-create

--- s1iw_catalogue/_version.py
+++ s1iw_catalogue/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '2026.6.13.dev79+g959337f7d.d20260703'
-__version_tuple__ = version_tuple = (2026, 6, 13, 'dev79', 'g959337f7d.d20260703')
+__version__ = version = '2026.6.13.dev90+g1c2ed4ca1.d20260703'
+__version_tuple__ = version_tuple = (2026, 6, 13, 'dev90', 'g1c2ed4ca1.d20260703')
 
 __commit_id__ = commit_id = None
--- s1iw_catalogue/web/__init__.py
+++ s1iw_catalogue/web/__init__.py
@@ -4,7 +4,7 @@ This module provides a FastAPI-based web interface to explore
 Sentinel-1 IW catalogues with interactive visualizations.
 """
 
-from s1iw_catalogue.web.app import app
+# from s1iw_catalogue.web.app import app
 from s1iw_catalogue.web.template_engine import CustomTemplates, get_templates
 
 __all__ = ["app", "get_templates", "CustomTemplates"]
